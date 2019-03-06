cce7cfc6b2c509b03fdc4ed1c0ac8675ccbc8c41
rinnguyen711 <rinnguyen711@gmail.com>
2019-03-06 11:12:01 +0700

    Update models.py

--- FaceDetection/models.py
+++ FaceDetection/models.py
@@ -6,7 +6,7 @@ from django.contrib.postgres.fields import ArrayField
 class FaceDetection(models.Model):
     image = models.ImageField()
     rects = ArrayField(ArrayField(models.FloatField(), max_length=4), null=True)
-
+    image_link = models.CharField(max_length=100, null=True)
 
 
 
